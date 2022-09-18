d143eb5a0a8ad30b17f9cfcb6562fbd7ad97231c
Gabriel Pedrotti da Silva <gabrielps2003@gmail.com>
2022-09-18 18:01:33 -0300

    feat: created login pagee

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:doctor_plus_app/views/content/content_page.dart';
 import 'package:flutter/material.dart';
+import 'package:doctor_plus_app/views/content/login_screen.dart';
 
 class App extends StatelessWidget {
   @override
@@ -10,7 +11,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: ContentPage(),
+      home: LoginScreen(),
     );
   }
 }
